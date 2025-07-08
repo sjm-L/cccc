16dddfbb095bfdc2ce5efa32bc8fb6d24b7e5964
sjm-L <shower1300@gmail.com>
2025-07-08 15:43:50 +0900

    문제1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Concurrent;
+﻿// 1. List<string> fruits = new List<string> { "apple", "banana", "cherry" }; 에서 "a"로 시작하는 과일만 필터링하려면 어떻게 해야 할까?
 
 namespace ConsoleApp1;
 
@@ -6,15 +6,12 @@ class Program
 {
   static void Main()
   {
-    List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
-    //<> 제네릭 넣을 필요가 있는지?
-    var evenNumbers = numbers.Where(n => n % 2 == 0);
-    //n => n % 2 == 0 는 n은 2로 나누어서 나머지가 0이 나오는 리스트 저장 
-    foreach (var num in evenNumbers)
-    //foreach 이건 모르겠지만 var num형을 만들고 거기다가 evnNumers을 저장 
-    //foreach은 컬랙션 요소를 하나식 꺼낼 때 ex: 리스트, 배여르 딕션너리
-    {
-      Console.WriteLine(num);
-    }
+    List<string> fruits = new List<string> { "apple", "banana", "cherry" };
+    var a = fruits.Where(fruits => fruits.StartsWith("a")).ToList();
+
+    foreach (var fruit in a)
+      {
+        Console.WriteLine(fruit);
+      }
   }
 }
--- obj/Debug/net9.0/c#_ex.AssemblyInfo.cs
+++ obj/Debug/net9.0/c#_ex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c7c30b4866c5514f86c2b8cfd9102c8357a7a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ccacd086fb7cdd331a6470c53de7381c0d2d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

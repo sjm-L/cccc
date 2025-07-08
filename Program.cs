05fb35213bf5954a197425f6117f14e6e7282641
sjm-L <shower1300@gmail.com>
2025-07-08 11:51:07 +0900

    ex

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, sum = 0;
+
+            while (n < 10000)
+            {
+                if (n % 3 == 0 || n % 5 == 0)
+                {
+                    sum += n;
+                }
+                n++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

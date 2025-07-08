06ff36410712a91b12e0f3decdbbc5792952f02d
sjm-L <shower1300@gmail.com>
2025-07-08 17:48:31 +0900

    commit

--- obj/Debug/net9.0/c#_ex.AssemblyInfo.cs
+++ obj/Debug/net9.0/c#_ex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ccacd086fb7cdd331a6470c53de7381c0d2d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dddfbb095bfdc2ce5efa32bc8fb6d24b7e5964")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#_ex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

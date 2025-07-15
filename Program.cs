bcf1c81ccaf5f40f2100208835d9176fbad45a22
sjm-L <shower1300@gmail.com>
2025-07-15 09:43:30 +0900

    다시 작성

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
 ﻿class Program
 {
-  public class Player
+  public class Quest<T>
   {
-    private int _gold;
-    public int Gold
-    {
-      set { _gold = value; }
-    }
+    public string Title { get; set; }
+    public T Reward { get; set; }
 
-    public void ShowGold()
+    public void ShowReward()
     {
-      Console.WriteLine($"Gold: {_gold}");
+      Console.WriteLine($"퀘스트 '{Title}'의 보상 : {Reward}");
     }
   }
 
   public static void Main(string[] args)
   {
-    Player P = new Player();
-    P.Gold = 500; //값 설정
-    P.ShowGold();
-    //Console.WriteLine(P.Gold); //에러
+    var goldQuest = new Quest<int> { Title = "도적 소탕", Reward = 1000 };
+    var itemQuest = new Quest<string> { Title = "마법서 찾기", Reward = "마법서" };
+    var xpQuest = new Quest<float> { Title = "훈현 참여", Reward = 25.25F };
+
+    goldQuest.ShowReward();
+    itemQuest.ShowReward();
+    xpQuest.ShowReward();
   }
 }
